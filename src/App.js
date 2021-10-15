68e454b2aa304c3c2b13da2ddc005cb208e144dd
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-15 17:10:22 +0300

    Create global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import { GlobalStyle } from "./GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
